6f12c97003df5f2c17a4bdfd8b744ca9788409d5
MingyouFang <estellafang@gmail.com>
2016-07-05 22:23:47 +1000

    Add project files.

---
+++ ASPMVCPractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPMVCPractice.Startup))]
+namespace ASPMVCPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
