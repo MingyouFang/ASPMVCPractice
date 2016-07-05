217e35fbee6dcefa055e1258fac6d844c9fadba5
MingyouFang <estellafang@gmail.com>
2016-07-06 00:38:30 +1000

    Step 2: Add HelloWorld controller

---
+++ ASPMVCPractice/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVCPractice.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/ 
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+        
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID is: " + ID);
+        }
+    }
+}
