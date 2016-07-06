cfb5620cedc1666bfa69c9f7e764ba67e0d4fc70
MingyouFang <estellafang@gmail.com>
2016-07-07 00:00:40 +1000

    Step 4: Add a Item model

---
+++ ASPMVCPractice/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace ASPMVCPractice.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool isEditable { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+    }
+}
