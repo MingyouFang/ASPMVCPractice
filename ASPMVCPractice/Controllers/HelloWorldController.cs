09fbdbd8da1adf506f110c975e45a069ce78b2c8
MingyouFang <estellafang@gmail.com>
2016-07-06 22:35:31 +1000

    Step 3: Add views

--- ASPMVCPractice/Controllers/HelloWorldController.cs
+++ ASPMVCPractice/Controllers/HelloWorldController.cs
@@ -9,15 +9,18 @@ namespace ASPMVCPractice.Controllers
     public class HelloWorldController : Controller
     {
         // GET: /HelloWorld/ 
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
         
         // GET: /HelloWorld/Welcome/ 
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID is: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
     }
 }
